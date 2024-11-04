744d8978b757a5d077cf030a246fcadebcc529fc
Enis B. Tuysuz <enisbtuysuz@gmail.com>
2024-11-04 18:56:15 +0300

    Basic map generation from osm file

--- Assets/Scripts/MapImporter.cs
+++ Assets/Scripts/MapImporter.cs
@@ -2,11 +2,41 @@ using UnityEngine;
 
 public class MapImporter : MonoBehaviour
 {
-    private OSMParser parser;
-    
+    [SerializeField] public GameObject NodePrefab;
+    OSMParser OsmParser;
+
     private void Start()
     {
-        parser = new OSMParser();
-        parser.LoadOSM("D:\\Games\\osm-unity-graph\\Assets\\Data\\map.osm");
+        OsmParser = new OSMParser();
+        OsmParser.LoadOSM("D:\\Games\\osm-unity-graph\\Assets\\Data\\map.osm");
+
+        foreach (var node in OsmParser.nodes.Values)
+        {
+            Vector2 position = node.position;
+            GameObject nodeObject = Instantiate(NodePrefab, position, Quaternion.identity);
+            nodeObject.name = $"Node_{node.id}";
+        }
+
+        foreach (var way in OsmParser.ways)
+        {
+            for (int i = 0; i < way.nodeRefs.Count - 1; i++)
+            {
+                if (OsmParser.nodes.TryGetValue(way.nodeRefs[i], out var startNode) &&
+                    OsmParser.nodes.TryGetValue(way.nodeRefs[i + 1], out var endNode))
+                {
+                    CreateEdge(startNode.position, endNode.position);
+                }
+            }
+        }
+    }
+
+    private void CreateEdge(Vector2 start, Vector2 end)
+    {
+        GameObject line = new GameObject("Line");
+        LineRenderer lr = line.AddComponent<LineRenderer>();
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
     }
 }
--- Assets/Scripts/OSMParser.cs
+++ Assets/Scripts/OSMParser.cs
@@ -2,9 +2,41 @@ using System.Xml.Linq;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using System.Collections.Generic;
+
+public class Node
+{
+    public string id { get; set; }
+    public Vector2 position { get; set; }
+
+    public Node(string id, Vector2 position)
+    {
+        this.id = id;
+        this.position = position;
+    }
+}
+
+public class Way
+{
+    public string id { get; set; }
+    public List<string> nodeRefs { get; set; }
+
+    public Way(string id)
+    {
+        this.id = id;
+        nodeRefs = new List<string>();
+    }
+}
 
 public class OSMParser
 {
+    public Dictionary<string, Node> nodes = new Dictionary<string, Node>();
+    public List<Way> ways = new List<Way>();
+    private Dictionary<string, Node> allNodes = new Dictionary<string, Node>();
+    private HashSet<string> referencedNodeIds = new HashSet<string>();
+    private Vector2 referencePoint = Vector2.zero;
+    private bool referencePointSet = false;
+
     public void LoadOSM(string filePath)
     {
         if (!File.Exists(filePath))
@@ -19,17 +51,46 @@ public class OSMParser
             string id = node.Attribute("id")?.Value;
             double lat = double.Parse(node.Attribute("lat")?.Value);
             double lon = double.Parse(node.Attribute("lon")?.Value);
+
+            if (!referencePointSet)
+            {
+                referencePoint = new Vector2((float)lon, (float)lat);
+                referencePointSet = true;
+            }
+
+            float normalizedLat = (float)(lat - referencePoint.y);
+            float normalizedLon = (float)(lon - referencePoint.x);
+
+            float scaleFactor = 5000f; // Move this to map importer
+
+            Vector2 position = new Vector2(normalizedLon, normalizedLat) / scaleFactor;
+            allNodes[id] = new Node(id, position);
         }
         
         foreach (var way in doc.Descendants("way"))
         {
             string id = way.Attribute("id")?.Value;
+            Way osmWay = new Way(id);
             var ndRefs = way.Descendants("nd").Select(nd => nd.Attribute("ref")?.Value).ToList();
-
-            foreach (var tag in way.Descendants("tag"))
+            if (ndRefs.First() == ndRefs.Last())
             {
-                string key = tag.Attribute("k")?.Value;
-                string value = tag.Attribute("v")?.Value;
+                // Skip closed paths
+                continue;
+            }
+            osmWay.nodeRefs.AddRange(ndRefs);
+            ways.Add(osmWay);
+
+            foreach (var nodeRef in ndRefs)
+            {
+                referencedNodeIds.Add(nodeRef);
+            }
+        }
+
+        foreach (var nodeId in referencedNodeIds)
+        {
+            if (allNodes.TryGetValue(nodeId, out var node))
+            {
+                nodes[nodeId] = node;
             }
         }
     }
